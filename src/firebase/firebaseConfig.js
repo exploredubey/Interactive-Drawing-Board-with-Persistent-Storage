f68598e4221c8ba161309732abbcbdbdca3515c9
Abhishek Dubey <40724238+exploredubey@users.noreply.github.com>
2024-11-12 12:33:39 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDCyRwOtNp3z2E6jW_t4qlCIhPhHoq5eVI",
-    authDomain: "writingboard-9f761.firebaseapp.com",
-    projectId: "writingboard-9f761",
-    storageBucket: "writingboard-9f761.appspot.com",
-    messagingSenderId: "197192933844",
-    appId: "1:197192933844:web:ce33a2c1bbe54d9abdf641",
-    measurementId: "G-GBRN0ZKQ9G"
+    apiKey: "##############################",
+    authDomain: "################################",
+    projectId: "#################",
+    storageBucket: "######################",
+    messagingSenderId: "#############",
+    appId: "#########################",
+    measurementId: "###################"
   };
   export default firebaseConfig;
